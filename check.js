0882acaed4c943a79bf9322c463fefe9080ceb4a
sherhard <goeasysher@gmail.com>
2023-02-06 23:03:44 +0800

    check list#1

---
+++ check.js
@@ -0,0 +1,14 @@
+let listButton=document.getElementById('listBtn')
+
+let check=document.getElementById('check_list');
+let list=document.getElementById('list');
+
+listButton.addEventListener('click',listBut);
+
+function listBut(){
+	let task=document.createElement('p');
+	task.innerHTML=check.value;
+	list.appendChild(task);
+}
+
+
