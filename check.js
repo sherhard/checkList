292aa08200549919fee720e6c516a9bc22e86eb4
sherhard <goeasysher@gmail.com>
2023-02-07 21:37:42 +0800

    project change

--- check.js
+++ check.js
@@ -5,10 +5,25 @@ let list=document.getElementById('list');
 
 listButton.addEventListener('click',listBut);
 
+
+
 function listBut(){
 	let task=document.createElement('p');
 	task.innerHTML=check.value;
 	list.appendChild(task);
+	check.value='';
+
+	task.addEventListener('click',del);
+	function del(){
+		list.removeChild(this)
+	}
+
+
+
+	
 }
+
+
+
 
 
